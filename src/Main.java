d3e948f3733978d9e80b80b2f62209c812fbbf0b
Zver <SvetlanaEvcheko@yandex.ru>
2024-07-19 10:41:50 +0300

    Initial commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car("car1", 4);
+        Car car2 = new Car("car2", 4);
+
+
+        Truck truck = new Truck("truck1",6);
+        Truck truck2 = new Truck("truck2",8);
+
+
+        Bicycle bicycle = new Bicycle("bicycle1", 2);
+        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceStation station = new ServiceStation();
+        ServiceStation.check(car, null, null);
+        ServiceStation.check(car2, null, null);
+        ServiceStation.check(null, bicycle, null);
+        ServiceStation.check(null, bicycle2, null);
+        ServiceStation.check(null, null, truck);
+        ServiceStation.check(null, null, truck2);
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,38 @@
+public class Vehicle {
+    public String modelName;
+    public int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+
+        }
+
+    }
+}
